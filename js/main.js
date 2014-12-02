83c760610a16a8c02e7d50cd8f978d11219a3bef
hbrittany <hogg0019@algonquinlive.com>
2014-12-02 08:25:03 -0500

    Removed parralax on bototm section

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ var mediaSectionOffset = $mediaSection.offset();
 $win.on('scroll', function () {
   var scrollPos = $win.scrollTop();
   var mediaPos = (scrollPos - mediaSectionOffset.top) / 2;
-
+//console.log(mediaSectionOffset, scrollPos);
 
   $introSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
-  $mediaSection.css('background-position', 'center ' + mediaPos + 'px');
+  //$mediaSection.css('background-position', 'center ' + mediaPos + 'px');
 });
 
