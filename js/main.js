cbe88a1e75a24626a473498243810688fd966c4d
hbrittany <hogg0019@algonquinlive.com>
2014-12-03 13:38:57 -0500

    removed gif

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 var $win = $(window);
 var $introSection = $('.intro-section');
-var $media = $('.media img');
 var $mediaSection = $('.media-section');
-var mediaSectionOffset = $mediaSection.offset();
+
 
 $win.on('scroll', function () {
   var scrollPos = $win.scrollTop();
-  var mediaPos = (scrollPos - mediaSectionOffset.top) / 2;
-//console.log(mediaSectionOffset, scrollPos);
 
-  $introSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
-  //$mediaSection.css('background-position', 'center ' + mediaPos + 'px');
+  $introSection.css('background-position', 'center ' + scrollPos / 1.5 + 'px');
+
 });
-
